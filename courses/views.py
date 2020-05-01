d3dfcd79f23ecc3ea38ddfc00132c9df955ab53e
Amit <amit.nayal24@gmail.com>
2020-05-01 18:12:31 +0530

    paginator

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Lecture, Section, Course
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def view_lecture(request, course_slug, lecturepk, lecture_slug):
-    lecture = get_object_or_404(Lecture, pk=lecturepk)
-    return render(request, 'courses/view_lecture.html', {'lecture': lecture})
+    lecture_list = get_object_or_404(Lecture, pk=lecturepk)
+    paginator = Paginator(lecture_list, 1)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        lecture = paginator.page(page)
+    except PageNotAnInteger:
+        lecture = paginator.page(1)
+    except EmptyPage:
+        lecture = paginator.page(paginator.num_pages)
+    context = {
+        "object_list": lecture,
+        "title": "List",
+        "page_request_var": page_request_var
+    }
+    return render(request, 'courses/view_lecture.html', {'lecture': context})
 
 
 def course_landing_page(request, course_slug):
