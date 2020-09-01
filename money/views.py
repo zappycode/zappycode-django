0c269ece314056bb582d027047eb4e4dc908ce2e
LordUttam <47154785+LordUttam@users.noreply.github.com>
2020-09-01 21:32:10 +0530

    Error on the money page and custom 404

--- money/views.py
+++ money/views.py
@@ -6,9 +6,14 @@ from .models import Month
 def home(request):
     months = Month.objects.order_by('-year', '-month')
     totals = Month.objects.aggregate(revenue=Sum('revenue'), expenses=Sum('expenses'))
-    profit = totals['revenue'] - totals['expenses']
-    return render(request, 'money/home.html',
-                  {'months': months, 'revenue': totals['revenue'], 'expenses': totals['expenses'], 'profit': profit, 'is_profit': profit > 0})
+    #profit = totals['revenue'] - totals['expenses']
+    profit = Month.objects.aggregate(Sum('revenue'), Sum('expenses'))
+    if profit is None:
+        return render(request, 'money/home.html',
+                  {'months': months, 'revenue': totals['revenue'], 'expenses': totals['expenses'], 'profit': profit})
+    else:
+        return render(request, 'money/home.html',
+                  {'months': months, 'revenue': 0, 'expenses': 0, 'profit': 0})
 
 
 def view_month(request, month_pk, month_slug):
