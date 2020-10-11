69dbf163403e8d33e5a5bdcf3db641c546efa96e
Brian Oliver <brianoliver@MacBook-Pro-3.local>
2020-10-11 03:27:04 -0500

    removed venv from python3 venv, updated .gitignore, added testing for tutorials app, added dynamic page titles and meta descriptions

--- tutorials/tests.py
+++ tutorials/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TestPage(TestCase):
+    def test_tutorials_listing_page(self):
+        response = self.client.get(reverse("tutorials:tutorials_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tutorials/tutorials_list.html')
+    
+    def test_tutorials_detail_page(self):
+        response = self.client.get(reverse("tutorials:tutorials_detail"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tutorials/tutorials_detail.html')
