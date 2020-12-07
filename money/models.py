399446c0b9b7b3d4359e2eac23902caa34d40e0e
Brian Oliver <semperindoctrina19@gmail.com>
2020-12-07 02:19:48 -0600

    added chart

--- money/models.py
+++ money/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 import calendar
+import json
 from django.utils.text import slugify
 
+class MonthManager(models.Manager):
+    def to_json(self):
+        """ Returns all objects as a list of JSON objects with format {"month": month, "mrr": mrr} """
+        qs = self.get_queryset().values("month", "mrr")
+        list_of_dicts = list(qs)
+        for single_dict in list_of_dicts:
+            single_dict["mrr"] = str(single_dict["mrr"])
+            single_dict["month"] = calendar.month_name[single_dict["month"]]
+        return json.dumps(list_of_dicts)
+        
 
 class Month(models.Model):
     month = models.IntegerField()
@@ -10,6 +21,9 @@ class Month(models.Model):
     revenue = models.DecimalField(max_digits=10, decimal_places=2)
     body = models.TextField()
     youtube_id = models.CharField(max_length=255, null=True, blank=True)
+    mrr = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    objects = MonthManager()
 
     def __str__(self):
         return self.title()
