e8c657e38958f7823e5c26331c897c9d83fe7aae
Nick <nick@zappycode.com>
2021-02-23 00:15:18 -0700

    Terminal short

--- zappycode/urls.py
+++ zappycode/urls.py
@@ -86,5 +86,6 @@ urlpatterns = [
                       url='https://www.udemy.com/macos-programming-for-ios-developers-mac-apps-os-x-cocoa/?couponCode=YOUTUBEMAC')),
                   path('pa/', RedirectView.as_view(url='https://www.pythonanywhere.com/?affiliate_id=006444cc')),
                   path('weekend/', RedirectView.as_view(url='https://zappycode.com/posts/15/python-in-a-weekend')),
+                  path('terminal/', RedirectView.as_view(url='https://zappycode.com/posts/17/terminal-tutorial')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
                                                                                          document_root=settings.STATIC_ROOT)
