33c113d9b3e7dfdadb20a9f29b15cf310483627f
Nick <nick@ZappyCode.com>
2023-09-30 13:17:59 +0100

    Oops forgot this :/

--- send/views.py
+++ send/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from send.models import SendIt
 
 # Create your views here.
 
 def sendit(request, redirect_uuid):
-	print(redirect_uuid)
-	return redirect("http://stackoverflow.com/")
+	sendIt = get_object_or_404(SendIt, uuid=redirect_uuid)
+	return redirect(sendIt.url)
